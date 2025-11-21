4c0a830b954a6db8220f684568356ea1ddce25f3
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-11-22 03:29:53 +0800

    fix(reports): fixes crash on reports screen

--- app/src/main/java/com/example/financialtrack/ui/reports/ReportsActivity.kt
+++ app/src/main/java/com/example/financialtrack/ui/reports/ReportsActivity.kt
@@ -1,21 +1,21 @@
 package com.example.financialtrack.ui.reports
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.financialtrack.R
-import com.example.financialtrack.databinding.ActivityNotificationBinding
-import com.example.financialtrack.databinding.ActivityReportsBinding
 
 class ReportsActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityReportsBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityReportsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnBackReports.setOnClickListener {
-            finish()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_reports)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
