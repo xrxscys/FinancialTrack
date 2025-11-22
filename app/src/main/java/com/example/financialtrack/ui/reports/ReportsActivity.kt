e34e0063f060ad00b4e1b04ab32d5b98e19d1ea9
Ivan Leopoldo <ivanleopoldoc@gmail.com>
2025-11-22 14:30:12 -0300

    feat(reports): adds header and overview

--- app/src/main/java/com/example/financialtrack/ui/reports/ReportsActivity.kt
+++ app/src/main/java/com/example/financialtrack/ui/reports/ReportsActivity.kt
@@ -6,16 +6,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.financialtrack.R
+import com.example.financialtrack.databinding.ActivityProfileBinding
+import com.example.financialtrack.databinding.ActivityReportsBinding
 
 class ReportsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReportsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_reports)
+        binding = ActivityReportsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnBack.setOnClickListener {
+            finish()
         }
     }
 }
